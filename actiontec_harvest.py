f03bc2a76c95ed32d1a031facef7a57f17850bc7
MikimotoH <MikimotoH@gmail.com>
2015-11-17 11:02:02 +0000

    upsert models that has no firmware downloads

--- actiontec_harvest.py
+++ actiontec_harvest.py
@@ -6,10 +6,10 @@ from harvest_utils import waitClickable, waitVisible, waitText, getElems, \
         getText,getNumElem,waitTextChanged,waitElem,\
         waitUntil,clickElem,getElemAttr,hasElem,waitUntilStable,\
         waitUntilA,mouseClickE,waitTextA,UntilTextChanged,mouseOver,elemWithText,\
-        goToUrl
+        goToUrl,isReadyState
 from selenium.common.exceptions import NoSuchElementException, \
         TimeoutException, StaleElementReferenceException, \
-        WebDriverException
+        WebDriverException, ElementNotVisibleException
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait,Select
 from selenium.webdriver.common.action_chains import ActionChains
@@ -22,13 +22,12 @@ import time
 from datetime import datetime
 import ipdb
 import traceback
-from my_utils import uprint,ulog
+from my_utils import uprint,ulog,getFuncName
 from contextlib import suppress
 
 driver,conn=None,None
 startTrail=[]
 prevTrail=[]
-modelName=''
 
 def glocals()->dict:
     """ globals() + locals()
@@ -42,13 +41,17 @@ def getScriptName():
     from os import path
     return path.splitext(path.basename(__file__))[0]
 
+def getTextA(elm:WebElement)->str:
+    waitUntil(lambda: elm.is_displayed() and elm.text,30,1)
+    return elm.text
+
 def retryA(statement, timeOut:float=6.2, pollFreq:float=0.3):
     timeElap=0
     while timeElap<timeOut:
         timeBegin=time.time()
         try:
             return statement()
-        except (StaleElementReferenceException,NoSuchElementException, StopIteration):
+        except (StaleElementReferenceException,NoSuchElementException, StopIteration, ElementNotVisibleException ):
             pass
         except Exception as ex:
             ulog('raise %s %s'%(type(ex),str(ex)))
@@ -85,13 +88,10 @@ def sql(query:str, var=None):
 
 def guessDate(txt:str)->datetime:
     """ txt = '2015-08-06' """
-    try:
-        m = re.search(r'\d{4}-\d{2}-\d{2}', txt)
-        return datetime.strptime(m.group(0), '%Y-%m-%d')
-    except Exception as ex:
-        ipdb.set_trace()
-        traceback.print_exc()
-        print('txt=',txt)
+    m = re.search(r'\d{4}-\d{2}-\d{2}', txt)
+    if not m:
+        return None
+    return datetime.strptime(m.group(0), '%Y-%m-%d')
 
 def guessVersion(txt:str)->str:
     """ 'Firmware Upgrade - GT784WN NCS01-1.0.13' """
@@ -102,71 +102,108 @@ def guessVersion(txt:str)->str:
         traceback.print_exc()
 
 def fileWalker():
-    global driver,prevTrail,modelName
+    global driver,prevTrail
     try:
+        waitUntil(isReadyState)
         prodName=waitText('#prodname')
+        ulog('prodName="%s"'%prodName)
+        modelName = waitText('#prodmodel')
+        ulog('modelName="%s"'%modelName)
         # click "Support"
         retryA(lambda: elemWithText('li.tab-link', 'Support').click())
         # expand "Downloads"
         waitClickable('div.accordion-section:nth-child(2) a').click()
+        pageUrl=driver.current_url
 
         # select  tables
         try:
-            tables = getElems('table.supp',15,1)
+            tables = getElems('table.supp',9,1)
         except TimeoutException:
             tables=None
         if not tables:
             ulog('no firmware download for "%s"'%modelName)
+            trailStr=str(prevTrail)
+            sql("INSERT OR REPLACE INTO TFiles (model,product_name,"
+                "page_url,tree_trail) VALUES"
+                "(:modelName, :prodName,"
+                ":pageUrl,:trailStr)",glocals())
+            ulog('UPSERT "%(modelName)s", "%(prodName)s", '%glocals())
             driver.back()
             return
         relDates = getElems('table.supp tr td:nth-child(1)')
         descs = getElems('table.supp tr td:nth-child(2)')
-        assert len(relDates)==len(descs)
-        pageUrl=driver.current_url
+        # assert len(relDates)==len(descs)
         startIdx = getStartIdx()
         numFiles=len(relDates)
         ulog('numFiles=%s'%numFiles)
+        bUpserted=False
         for idx in range(startIdx, numFiles):
             fwDate=guessDate(relDates[idx].text)
+            if not fwDate:
+                ulog('bypass idx=%s'%idx)
+                continue
             fileUrl=descs[idx].find_element_by_css_selector('a')
+            fileName=fileUrl.text
+            ulog('fileName="%s"'%fileName)
+            if 'firmware' not in fileName.lower():
+                ulog('bypass idx=%s'%idx)
+                continue
             fwVer = guessVersion(fileUrl.text)
-            fileUrl=fileUrl.get_attibute('href')
+            fileUrl=fileUrl.get_attribute('href')
 
             fwDesc=descs[idx].text.strip()
             trailStr=str(prevTrail+[idx])
             ulog('trail=%s'%trailStr)
-            sql("INSERT OR REPLACE INTO TFiles (model,"
+            sql("INSERT OR REPLACE INTO TFiles (model,product_name,"
                 "fw_date, fw_ver, fw_desc, "
                 "page_url,file_url,tree_trail) VALUES"
-                "(:modelName,  "
+                "(:modelName, :prodName,"
                 ":fwDate,:fwVer,:fwDesc,"
                 ":pageUrl,:fileUrl,:trailStr)",glocals())
-            ulog('UPSERT "%(modelName)s", "%(fwDate)s", '
+            ulog('UPSERT "%(modelName)s", "%(prodName)s", "%(fwDate)s", '
                 ' "%(fwVer)s", %(fileUrl)s'%glocals())
+            bUpserted=True
+
+        if not bUpserted:
+            trailStr=str(prevTrail)
+            sql("INSERT OR REPLACE INTO TFiles (model,product_name,"
+                "page_url,tree_trail) VALUES"
+                "(:modelName, :prodName,"
+                ":pageUrl,:trailStr)",glocals())
+            ulog('UPSERT "%(modelName)s", "%(prodName)s", '%glocals())
+
         driver.back()
         return
     except Exception as ex:
         ipdb.set_trace()
         traceback.print_exc()
         driver.save_screenshot(getScriptName()+'_'+getFuncName()+'_exc.png')
-        
+
+
+mkts = ['#myHeader1','#myHeader2','#myHeader3','#myHeader4']
 def modelWalker():
-    global driver,prevTrail,modelName
+    global driver,prevTrail
+    CSS=driver.find_element_by_css_selector
+    CSSs=driver.find_elements_by_css_selector
     try:
-        models = getElems('#newboxes1 a')
+        retryA(lambda:CSS(mkts[prevTrail[-1]]).click())
+        models = CSSs('#newboxes%d a'%(prevTrail[-1]+1))
         numModels=len(models)
         ulog('numModels=%s'%numModels) 
         startIdx = getStartIdx()
         for idx in range(startIdx,numModels):
-            modelName = models[idx].text
-            ulog('enter %s,"%s"'%(idx,modelName))
+            waitUntil(lambda: models[idx].is_displayed(),5)
+            if not models[idx].text:
+                ulog('bypass idx=%s'%idx)
+                continue
+            ulog('enter %s,"%s"'%(idx,models[idx].text))
             prevTrail+=[idx]
-            models[idx].click()
+            retryA(lambda: models[idx].click())
             fileWalker()
             prevTrail.pop()
-            cats = getElems('#myHeader1,#myHeader2,#myHeader3')
-            cats[prevTrail[-1]].click()
-            models = getElems('#newboxes1 a')
+            waitUntil(isReadyState)
+            retryA(lambda:CSS(mkts[prevTrail[-1]]).click())
+            models = CSSs('#newboxes%d a'%(prevTrail[-1]+1))
             assert numModels == len(models)
     except Exception as ex:
         ipdb.set_trace()
@@ -175,19 +212,15 @@ def modelWalker():
 
 def marketWalker():
     global driver,prevTrail
-    rootUrl='http://www.actiontec.com/support/'
+    CSS=driver.find_element_by_css_selector
     try:
-        goToUrl(rootUrl)
-        cats = getElems('#myHeader1,#myHeader2,#myHeader3')
         startIdx=getStartIdx()
-        numCats = len(cats)
+        numCats = len(mkts)
         for idx in range(startIdx, numCats):
             ulog('select %s'%idx)
-            cats[idx].click()
             prevTrail+=[idx]
             modelWalker()
             prevTrail.pop()
-            cats = getElems('#myHeader1,#myHeader2,#myHeader3')
     except Exception as ex:
         ipdb.set_trace()
         traceback.print_exc()
@@ -196,6 +229,7 @@ def marketWalker():
 
 def main():
     global startTrail,prevTrail, driver,conn
+    rootUrl='http://www.actiontec.com/support/'
     try:
         startTrail = [int(re.search(r'\d+', _).group(0)) for _ in sys.argv[1:]]
         uprint('startTrail=%s'%startTrail)
@@ -219,6 +253,7 @@ def main():
         driver=harvest_utils.getFirefox()
         harvest_utils.driver=driver
         prevTrail=[]
+        goToUrl(rootUrl)
         marketWalker()
         driver.quit()
         conn.close()
